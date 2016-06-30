c191e4c11b41babd73c4e8813727c808eabfc037
Philip Castiglione <philipcastiglione@gmail.com>
2016-06-30 19:12:10 +1000

    hello all the worlds

--- Hello World/HelloWorld.java
+++ Hello World/HelloWorld.java
@@ -6,5 +6,7 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        System.out.println("Hello Dlrow!");
+        System.out.println("全世界，大家好！");
     }
 }
