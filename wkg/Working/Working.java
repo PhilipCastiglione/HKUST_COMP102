a3ec6de1f56738aaedd92a301c80fa591d40d348
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-13 14:13:38 +1000

    branching statements

--- wkg/Working/Working.java
+++ wkg/Working/Working.java
@@ -6,28 +6,21 @@
  * @version (a version number or a date)
  */
 public class Working
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Working
-     */
-    public Working(int x)
-    {
-        // initialise instance variables
-        System.out.print("arg x is a: ");
-        //System.out.println(x.getClass());
-        // this has a compile error "int cannot be dereferenced" referring to x.getClass()
-        x = x;
-    }
-    
+{    
     public static void main(String[] args) {
-        Working w = new Working(13);
+        int score = 62;    //initialize value according to question
+        char grade;
 
-        System.out.println("w is a: " + w.getClass());
+        switch(score/10) {
 
-        //System.out.println("w.x is a: " + w.x.getClass());
-        // this has a compile error "int cannot be dereferenced" referring to w.x.getClass()
+            case 10: grade = 'S'; break;
+            case  9: grade = 'A'; 
+            case  8: grade = 'B'; 
+            case  7: grade = 'C'; 
+            case  6: grade = 'D'; 
+            case  5: grade = 'E'; break;
+            default: grade = 'F';
+        }
+        System.out.println(grade);
     }
 }
