e21bb32c45c3eace694c8de6c34df4b609870eed
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-13 15:19:34 +1000

    testing conditional syntax compilation

--- wkg/Working/Working.java
+++ wkg/Working/Working.java
@@ -7,20 +7,16 @@
  */
 public class Working
 {    
+    int x = 2;
+    
+    public void derp() {
+        int x = 3;
+        
+        System.out.println(this.x);
+        System.out.println(x <> this.x);
+    }
+    
     public static void main(String[] args) {
-        int score = 62;    //initialize value according to question
-        char grade;
-
-        switch(score/10) {
-
-            case 10: grade = 'S'; break;
-            case  9: grade = 'A'; 
-            case  8: grade = 'B'; 
-            case  7: grade = 'C'; 
-            case  6: grade = 'D'; 
-            case  5: grade = 'E'; break;
-            default: grade = 'F';
-        }
-        System.out.println(grade);
+        derp();
     }
 }
