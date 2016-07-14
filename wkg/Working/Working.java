3acdddacb3aff81efcaee22ad605424a1df885f0
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-15 09:33:04 +1000

    more working changes for loops etc

---
+++ wkg/Working/Working.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Working here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Working
+{    
+    public static void main(String[] args) {
+        int n1 = 4;
+        int n2 = 2;
+        
+        int t = 1;
+        
+        for (int i = n1; i <= n2; i++) {
+            t *= i;
+        }
+        System.out.println(t);
+    }
+}
