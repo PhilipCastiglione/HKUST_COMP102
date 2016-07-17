f8109b9b86ae86207b74f7fb7bbc5a98b3640178
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-17 14:56:18 +1000

    added working file for testing java lang features

---
+++ part_two/wkg/Wkg/Sup.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Sup here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Sup
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Sup
+     */
+    public Sup(int a)
+    {
+        // initialise instance variables
+        x = a;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
---
+++ part_two/wkg/Wkg/Wkg.java
@@ -0,0 +1,11 @@
+public class Wkg extends Sup
+{
+    
+    public Wkg() {
+        System.out.println(getValue());
+    }
+    
+    public static void main(String[] args) {
+        Wkg w = new Wkg();
+    }
+}
