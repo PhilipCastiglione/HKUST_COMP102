a9042161c55f19eaaef418fcbbaaab140091f2c5
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-18 21:13:54 +1000

    recursion

--- part_two/wkg/Wkg/Wkg.java
+++ part_two/wkg/Wkg/Wkg.java
@@ -2,15 +2,29 @@ import java.util.Scanner;
 
 public class Wkg extends Sup
 {
+    public static int f2(int a, int b) {
+    
+        if (b >= 1)
+            return f2(a + 1, b - 1);
+        else
+            return a;
+    }
+    
+    public static int f3(int n) {
+      
+        if (n == 0)
+            return 1;
+      
+        else if (n < 10 && n > -10)
+            return 0;
+      
+        else      
+            return f3(n / 10) + f3(n % 10);
+    }
  
     public static void main (String[] args) {
         //Scanner s = new Scanner();
-        Scanner scanner = new Scanner("1 A 2 B");
-        
-        System.out.println(scanner.nextInt()); // statement 1
-        System.out.println(scanner.nextInt()); // statement 2
-                
-        scanner.close();
+        System.out.println(Wkg.f3(10200));
 
  
     }
