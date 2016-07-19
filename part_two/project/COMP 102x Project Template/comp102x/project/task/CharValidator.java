11219775bc3fc27810976732d5ecf70c64273dbd
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-19 22:26:55 +1000

    project task 1 answer

--- part_two/project/COMP 102x Project Template/comp102x/project/task/CharValidator.java
+++ part_two/project/COMP 102x Project Template/comp102x/project/task/CharValidator.java
@@ -1,12 +1,9 @@
 package comp102x.project.task;
 
 public class CharValidator {
-	
+   
 	public boolean validateChar(char c) {
-		
-		// Please write your code after this line
-		
-		return false; // This line should be modified or removed after finising the implementation of this method.
+	    return Character.isDigit(c) || Character.isLetter(c);
 	}
 
 }
