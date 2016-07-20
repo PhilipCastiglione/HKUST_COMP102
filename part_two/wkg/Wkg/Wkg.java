bd2cecf6ab2911746ade388cfbf2576f4c2a5f2d
Philip Castiglione <philipcastiglione@gmail.com>
2016-07-20 10:26:13 +1000

    stacks!

--- part_two/wkg/Wkg/Wkg.java
+++ part_two/wkg/Wkg/Wkg.java
@@ -1,19 +1,17 @@
-import java.util.Scanner;
+import java.util.Stack;
 
 public class Wkg extends Sup
 {
-    int nCount = 0;
-    
-    public int fibonacci(int n) {
-        nCount++;
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-    
+
+   
     public static void main (String[] args) {
-        Wkg w = new Wkg();
-        w.fibonacci(8);
-        System.out.println(w.nCount);
+        Stack s = new Stack<Integer>();
+        s.push(1);
+        s.push(4);
+        s.push(2);
+        s.push(3);
+        s.push(8);
+        
+        System.out.println(s);
     }
 }
